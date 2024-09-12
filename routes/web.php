23ea4d002ba41117d2350723d07023ae0987e5a5
Annisa P <annisaprissilya@gmail.com>
2024-09-12 09:46:44 +0700

    jobsheet 3 PWL_POS

--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/level', [LevelController::class,'index']);
 
+Route::get('/kategori',[KategoriController::class, 'index']);
+
+Route::get('/user',[UserController::class, 'index']);
 
 
